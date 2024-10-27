4315cb46d23887d1c85fd957cbc8afce1e9c9c21
hasib105 <hasibahmed1054@gmail.com>
2024-10-27 23:42:37 +0600

    add mantine templates

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './App.css';
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
+import { theme } from "./theme";
 
-
-
-const App = () => {
-  const [data, setData] = useState<string | null>(null);
-
-  useEffect(() => {
-    axios
-      .get('http://localhost:8000/api/example/')
-      .then((response) => setData(response.data.message))
-      .catch((error) => console.error(error));
-  }, []);
-
-  return (
-    <div className='bg-red-500 text-start flex justify-center'>
-      {data ? <h1>{data}</h1> : <h1>Loading...</h1>}
-    </div>
-  );
-};
-
-export default App;
+export default function App() {
+  return <MantineProvider theme={theme}>App</MantineProvider>;
+}
